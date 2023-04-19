57720c01401807d7e4941f6388a06b05c0369173
Jan Kozma <jan.kozma@unob.cz>
2023-04-19 08:11:45 +0200

    RayTable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import HonzaTable from './components/HonzaTable';
+import Table from './components/RayTable';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Place your HonzaTable component here */}
         <HonzaTable />
+        <RayTable />
       </header>
     </div>
   );
